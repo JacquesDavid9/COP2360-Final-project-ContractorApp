533ce7b0e97d9ad7ab5911e1516a0b50798a6451
azer0503 <87244486+azer0503@users.noreply.github.com>
2026-07-22 15:37:41 -0400

    Updated Contractor.cs

--- Contractor.cs
+++ Contractor.cs
@@ -1,22 +1,29 @@
-// Member 2 will replace this with the real implementation. Base class holding contractor name, number, and start date.
- 
-using System;
- 
+﻿using System;
+
+
 namespace ContractorApp
 {
     public class Contractor
     {
+        // Constractor Accessors & Mutators
         public string ContractorName { get; set; }
-        public string ContractorNumber { get; set; }
-        public string ContractorStartDate { get; set; }
- 
-        public Contractor() { }
- 
-        public Contractor(string name, string number, string startDate)
+        public int ContractorNumber { get; set; }
+        public DateOnly ContractorStartDate { get; set; }
+
+        //Contractor Constructors
+        public Contractor()
+        {
+            ContractorName = "";
+            ContractorNumber = 0;
+            ContractorStartDate = DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        public Contractor(string name, int number, DateOnly startDate)
         {
             ContractorName = name;
             ContractorNumber = number;
             ContractorStartDate = startDate;
         }
     }
+
 }
